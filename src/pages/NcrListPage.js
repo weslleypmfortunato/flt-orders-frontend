fe8749585286f0af3f81ebf3400570e822156f52
weslleypmfortunato <weslley.m.fortunato@gmail.com>
2024-02-10 13:31:25 -0800

    added table at NcrListPage

--- src/pages/NcrListPage.js
+++ src/pages/NcrListPage.js
@@ -44,34 +44,45 @@ const NcrListPage = () => {
 
   return (
     <div>
-    <Navbar />
-      <h1 className="mb-4 mt-4 text-2xl font-semibold">NCR List</h1>
-      <Link to='/ncr/new'>
+      <Navbar />
+      <div className="flex flex-col items-center">
+        <h1 className="mb-4 mt-4 text-2xl font-semibold">NCR List</h1>
+        <Link to='/ncr/new'>
           <button
             type="button"
             className={`border-1 rounded text-lg px-2 bg-blue-500 text-white font-semibold my-4 w-52 h-9 transition duration-500 transform hover:scale-105 hover:border hover:border-blue-900 shadow-2xl shadow-blue-900 ${loggedInUser.user.level !=="admin" && "hidden"}`}>
             Add New NCR
           </button>
-      </Link>
-      <div className="flex flex-col items-start ml-96 px-5">
-      {ncrs.length > 0 && ncrs.map(ncr => {
-        return (
-          <div key={ncr._id}>
-            <div>
-              <Link to={`/ncr/${ncr._id}`}>
-                <p className="m-0.5">{capitalizeFirstLetter(ncr.title)}</p>
-              </Link>
-            </div>
-          </div>
-        )
-      })}
-      </div>
-      <div>
-      </div>
-      <div>
-        <Link to={`/home`}>
-          <p className="m-0.5 mt-5">Home</p>
         </Link>
+        <div className="px-5">
+          <table className="border-collapse border">
+            <thead>
+              <tr>
+                <th className="border px-4 py-2">TITLE</th>
+                <th className="border px-4 py-2">CREATOR</th>
+                <th className="border px-4 py-2">DATE</th>
+              </tr>
+            </thead>
+            <tbody>
+              {ncrs.length > 0 && ncrs.map(ncr => (
+                <tr key={ncr._id} className="h-6">
+                  <td className="border border-blue-500 px-4 py-2 text-left">
+                    <Link to={`/ncr/${ncr._id}`}>
+                      {capitalizeFirstLetter(ncr.title)}
+                    </Link>
+                  </td>
+                  <td className="border border-blue-500 px-4 py-2">{capitalizeFirstLetter(ncr.creator)}</td>
+                  <td className="border border-blue-500 px-4 py-2">{ncr.ncrDate?.substring(0, 10) || "No Date Provided"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <Link to={`/home`}>
+            <p className="m-0.5 mt-5">Home</p>
+          </Link>
+        </div>
       </div>
     </div>
   )
